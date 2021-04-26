23681391a7e83bb238c6c41e348e99afba33e83d
nqmt <to.nqmt@gmail.com>
2021-04-26 10:07:27 +0700

    add redis state & retry

--- server-sent-event/api-receive.js
+++ server-sent-event/api-receive.js
@@ -2,21 +2,32 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const EventSource = require('eventsource')
+const redis = require("redis");
+const client = redis.createClient();
+const { promisify } = require("util");
+
+client.on("error", console.error);
+client.set("key", "redis state", redis.print);
+
+const getAsync = promisify(client.get).bind(client);
 
 app.get('/', async function (req, res) {
-  console.log(EventSource)
   const eventSource = new EventSource('http://localhost:6001/event')
 
-  eventSource.addEventListener('message', function (event) {
-    console.log(event)
+  eventSource.addEventListener('message', async function (event) {
+    const state = await getAsync('key')
+    console.log(state, event)
   })
   eventSource.addEventListener('stop', function (event) {
     console.log('stop', event)
     eventSource.close()
   })
   eventSource.addEventListener('error', function (err) {
-    console.error(err);
-    eventSource.close()
+    // default retry every 1 second
+    console.error(err, new Date());
+
+    // for not require retry
+    // eventSource.close()
   })
 
   return res.status(200).send('success')
--- server-sent-event/server.js
+++ server-sent-event/server.js
@@ -1,37 +1,39 @@
 const express = require('express');
 const fs = require('fs');
-const app =express();
+const app = express();
 
-app.get('/event', async function(req,res) {
+app.get('/event', async function (req, res) {
   console.info('client connect event', new Date());
-  
+
   res.set({
     'Cache-Control': 'no-cache',
     'Content-Type': 'text/event-stream',
     'Connection': 'keep-alive'
   });
   res.flushHeaders();
-  
+
   // [Note] for set default retry
   // res.write('retry: 50000\n\n');
-  
+
   let i = 0
   while (true) {
     await new Promise(resolve => setTimeout(resolve, 500));
-    
+
     if (i === 10) {
       // [Note] Note use this method for stop connection or check specific data by parser
       res.write(`event: stop\n`);
       res.write(`data: stop\n`);
       res.write(`id: ${i}\n\n`);
-      
+
       i++;
       continue;
     }
-    
+
+    // set retry 3s
+    res.write(`retry: 3000\n\n`);
     res.write(`data: ${i} ${new Date()}\n`);
     res.write(`id: ${i}\n\n`);
-    
+
     i++
   }
 });
